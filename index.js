6d60db5dac09a6b515b3c1e01b0bfd09633eeb75
virendrakumar65 <virendra.kumar@numbertheory.ai>
2019-10-05 13:22:19 +0530

    simple node with test

---
+++ index.js
@@ -0,0 +1,12 @@
+//importing node framework
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+ res.send('hello world');
+});
+
+app.listen(process.env.PORT || 3000,function(){
+    console.log("app is runnign on port : 3000");
+});
+module.exports = app;
